47a7b1487e23c034dfc64091dcc9712c22e58d64
natalie <22222885+espeon@users.noreply.github.com>
2024-12-20 15:48:50 -0600

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
