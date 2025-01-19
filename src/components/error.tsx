7bbc24d44a5c01ba2ab2a28ae1d5fe17685caeb2
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-01-19 01:33:09 -0600

    bug: limit width of error so it can wrap fine

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -9,7 +9,7 @@ export default function ShowError({ error }: { error: Error }) {
     <div className="flex flex-col min-h-screen justify-center items-center gap-4">
       <div className="flex flex-col gap-2 items-center">
         <CircleAlert className="text-red-500" width={48} height={48} />
-        <div className="h-min">Error: {error.message}</div>
+        <div className="h-min text-wrap break-words max-w-md w-full text-center">Error: {error.message}</div>
       </div>
       <div className="flex flex-row gap-2 items-center">
         <Button variant="secondary" onClick={() => router.history.back()}>
